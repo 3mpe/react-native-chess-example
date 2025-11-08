f56bdeb21fd85b853dbc8d4a0e6330d2006cd1bb
Emre Vatansever <vatanseveremre90@gmail.com>
2025-11-08 16:33:23 +0300

    Refactor: Remove unused components and services
    - Deleted `styled.js` for `SpecialForMe` component as it is no longer needed.
    - Removed `index.js` for `Special` component.
    - Cleaned up `index.js` in `components` directory by removing references to deleted components.
    - Removed various services including:
      - `_activities.service.js`
      - `_consents.service.js`
      - `_mock.service.js`
      - `_notification.service.js`
      - `_otp.service.js`
      - `_qr.service.js`
      - `_roommate.service.js`
      - `_transportation.service.js`
      - `chatbot.service.js`
      - `globalSettings.service.js`
      - `identity.service.js`
      - `profile.service.js`
      - `transfer.service.js`
    - Updated `index.js` in `services` directory to reflect the removal of services.

--- src/services/chatbot.service.js
+++
@@ -1,9 +0,0 @@
-import API from './api';
-import URLS from './URLS';
-
-const ChatbotService = {
-  messageList: () => API.get(URLS.CHATBOT.MESSAGE_LIST),
-  postMessage: data => API.post(URLS.CHATBOT.CHAT, data),
-};
-
-export default ChatbotService;
